0d6d476c53816d71560ebad7cd4dd60e46cb0e60
abdur-shobur <abdur.shobur.me@gmail.com>
2026-01-31 00:20:15 +0600

    Check API endpoint

--- store/features/api/apiSlice.ts
+++ store/features/api/apiSlice.ts
@@ -60,6 +60,8 @@ export const getApiBaseUrl = () => {
 	const apiDomain = env.baseAPI.replace(/^https?:\/\//, ''); // storeeb.com
 	const subdomain = parts[0]; // two
 
+	console.log({ apiDomain, subdomain, parts });
+
 	if (parts.length === 2 && subdomain) {
 		return `https://${subdomain}.${apiDomain}/api`;
 	}
